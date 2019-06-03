f11cbaa46bbb1adfd65987abd09bcd9ef18cbaa5
Gustakitos <luisgustavolg1033@gmail.com>
2019-06-03 19:53:23 -0300

    Complete project

--- spec/system/exchanges_index_system_spec.rb
+++ spec/system/exchanges_index_system_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Exchange Currency Process", :type => :system, js: true do
   it "exchange value" do
-    visit '/'
+    visit '/exchanges/index'
     within("#exchange_form") do
       select('EUR', from: 'source_currency')
       select('USD', from: 'target_currency')
       fill_in 'amount', with: rand(1..9999)
     end
-    click_button 'CONVERTER'
+    click_button 'Converter'
 
     # save_and_open_page
     expect(page).to have_content("value")
