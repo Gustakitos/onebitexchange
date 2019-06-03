9b0cecefacc884d06b5397fb093f90eca5546872
Gustakitos <luisgustavolg1033@gmail.com>
2019-06-03 20:49:12 -0300

    added tether and bootstrap imported'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= bootstrap/dist/js/bootstrap
+//= require tether/dist/js/tether
+//= bootstrap/dist/js/bootstrap.bundle
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
