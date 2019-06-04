de5f57cd48c3e806c25e62bb9c48c63fbb9c9615
Gustakitos <luisgustavolg1033@gmail.com>
2019-06-04 01:24:48 -0300

    fix route and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'exchanges#index'
   get 'exchanges/index'
   get 'exchanges/convert'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
